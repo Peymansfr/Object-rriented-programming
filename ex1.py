1dfd6b27013393c3273d1bd1c1c5667978359d18
Peymansfr <peymansfr72@gmail.com>
2025-02-27 21:09:40 +0200

    push ex1

---
+++ ex1.py
@@ -0,0 +1,7 @@
+"""
+ 1. Explanation of concepts
+a. Encapsulation: Restricting direct access to object data, allowing controlled access via methods.
+b. Client: The code or user that interacts with an object or system.
+c. Data attributes: Variables stored within an object, representing its state.
+d. Instance: A specific object created from a class.
+"""
